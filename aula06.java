664dba8a9997a5507e672e1fd148aa24ddf22eb7
ereism1 <eduardomreis2@gmail.com>
2024-04-20 05:52:47 -0300

    remoto

---
+++ aula06.java
@@ -0,0 +1,13 @@
+package aula06;
+public class aula06{
+    public static void main (String[] args){
+        controleRemoto c = new controleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.ligarMudo();
+        c.abrirMenu();
+        c.play();
+        
+
+    }
+}
